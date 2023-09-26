5fcd1362dc3e017c0fce563bfb1494a243fb9e04
eril1024 <eril1094@naver.com>
2023-09-26 15:49:56 +0900

    sss

--- src/main/java/com/eating/application/EatingApplication.java
+++ src/main/java/com/eating/application/EatingApplication.java
@@ -8,7 +8,7 @@ public class EatingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EatingApplication.class, args);
-		System.out.println("hi");
+		System.out.println("hiss");
 	}
 
 }
